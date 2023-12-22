d9f37d56ebc0281a4cd9160cf743609797b28270
he.wenyao <he.wenyao@nint.com>
2023-12-22 11:42:03 +0800

    FIX

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ScrollProgress } from "@/components/progress";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600"],
@@ -29,6 +30,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange>
           <main>{children}</main>
+          <Analytics />
           <ScrollProgress />
         </ThemeProvider>
       </body>
