840d8d4b1116bea86cda0a4cf585c29f81e58e50
techiehe <huala.fun@outlook.com>
2024-06-09 23:57:05 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Desc from "@/components/Desc.mdx";
-import Skill from "@/components/Skill";
+import Skill from "@/components/Skill.mdx";
 
 export default function Home() {
   return (
-    <div className="mt-16 mx-4 lg:mb-0  lg:w-full lg:max-w-3xl prose">
+    <div className="mt-16 mx-4 lg:mb-0  lg:w-full lg:max-w-3xl prose select-none">
       <Desc />
-      <Skill skill="Next.js" />
+      <Skill />
     </div>
   );
 }
